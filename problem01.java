0a00b3f8443ab3dde95b7a10a6e98d64c02df787
Monilito Castro <monilito.castro@my.und.edu>
2015-12-06 01:34:19 -0600

    ONWARD

--- problem01.java
+++ problem01.java
@@ -8,25 +8,27 @@ LEVEL 2
 import java.lang.Integer;
 
 public class problem01{
-	public static void main(String args[]){
-		if(args.length<1){
-			System.out.println("Usage: java problem01 1000");
-			System.exit(0);
-		}
-		int x, y;
-		x = 1; y = 1;
-		int upperBound = Integer.parseInt(args[0])-1;
-		int init=upperBound-(upperBound%3);
-		int accumulate = 0;
-		for(x=init; x > 0; x-=3){
-			accumulate += x;
-		}
-System.out.println("accumulate = "+accumulate);
-		init = upperBound-(upperBound%5);
-		for(y=init; y > 0; y-=5){
-			accumulate += y;
-		}
-		System.out.println("The sum of all multiples of 3 or 5 below "+ upperBound +" is " + accumulate);
-	}
+ public static void main(String args[]){
+  if(args.length<1){
+   System.out.println("Usage: java problem01 1000");
+   System.exit(0);
+  }
+  int x, y;
+  x = 1; y = 1;
+  int upperBound = Integer.parseInt(args[0])-1;
+  
+  int accumulate = 0;
+  int init = upperBound-(upperBound%5);
+  upperBound++;
+  for(y=init; y > 0; y-=5){
+   if(y%3!=0)accumulate += y;
+  }
+  
+  init=upperBound-(upperBound%3);
+  for(x=init; x > 0; x-=3){
+   accumulate += x;
+  }
+  System.out.println("The sum of all multiples of 3 or 5 below "+ upperBound +" is " + accumulate);
+ }
 
 }
